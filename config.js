e41f105e27a362221280d186f9a7263cc067fa89
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-04-18 12:04:37 +0000

    .

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
   ownerIds: "1081995719210172497",
   avatarURL: "",
   currency: "<:cattypaw:1227841175671541801>",
-  mongoURL: process.env.mongoURL || readdirSync('mongoURL.txt', 'utf-8'),
+  mongoURL: "mongodb+srv://Blackness36:Blackness697@cattypal.931cn9t.mongodb.net/",
   YOUTUBE_API_KEY: process.env.YoutubeApiKey
 };
