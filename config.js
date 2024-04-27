387780143f841102458f0552cb39f22be0be04a2
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-04-27 14:24:59 +0000

    .

--- config.js
+++ config.js
@@ -11,11 +11,11 @@ module.exports = {
   token: process.env.token || readFileSync('token.txt', 'utf-8'),
   getPrefix, // Exporting the function to fetch prefix dynamically
   clientId: process.env.CLIENT_ID,
-  ownerIds: ["1081995719210172497", "1229341293176557570"], // Array of owner IDs
+  ownerIds: ["1081995719210172497", "1229341293176557570", "1153611682250227764"], // Array of owner IDs
   avatarURL: "",
-  currency: "<:cattypal:1232348823729213470>",
-  coinflip: "<a:coinflip:1232349729128579143>",
-  coinflip2: "<:coinflip2:1232351041186697246>",
+  currency: "<:cattypal:1233779228617740309>",
+  coinflip: "<a:coinflip:1233779307302621267>",
+  coinflip2: "<:coinflip2:1233779443001200670>",
   mongoURL: "mongodb+srv://Blackness36:Blackness697@cattypal.931cn9t.mongodb.net/",
   YOUTUBE_API_KEY: process.env.YoutubeApiKey
 };
