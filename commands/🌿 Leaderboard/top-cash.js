f92d34095d8d55db92043faf676a96d99fa88bfd
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-04-28 03:23:54 +0000

    .

--- commands/🌿 Leaderboard/top-cash.js
+++ commands/🌿 Leaderboard/top-cash.js
@@ -15,20 +15,23 @@ module.exports = {
           return msg.reply('Please provide a valid limit of users (1-100) to display.');
         }
       }
-
+      // Find the author's rank
+      const authorIndex = topUsers.findIndex(user => user.userId === msg.author.id);
+      const authorRank = authorIndex !== -1 ? authorIndex + 1 : 'N/A';
+      
       const topUsers = await User.find().sort({ balance: -1 }).limit(limit);
 
       if (topUsers.length === 0) {
         return msg.reply('No users found.');
       }
 
-      let leaderboard = `Top ${limit} Global Users by Balance:\n\n`;
+      let leaderboard = `Top ${limit} Global Users by Balance:\nYour rank is #${authorRank}\n\n`;
 
       for (let i = 0; i < topUsers.length; i++) {
         const user = await msg.client.users.fetch(topUsers[i].userId);
         if (!user) continue; 
 
-        leaderboard += `${i + 1}. **${user.username}** - **__${topUsers[i].balance.toLocaleString()}__** ${currency} CP coins\n--------------------------------------------\n`;
+        leaderboard += `#${i + 1}. **${user.username}** - **__${topUsers[i].balance.toLocaleString()}__** ${currency} CP coins\n--------------------------------------------\n`;
       }
 
       msg.channel.send(leaderboard);
