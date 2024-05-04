8c8644877b4f150d22d252da932c30c08197fa5e
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-05-04 09:48:24 +0000

    .

--- commands/🍀 Developer-only/add-coins.js
+++ commands/🍀 Developer-only/add-coins.js
@@ -5,24 +5,24 @@ module.exports = {
   name: 'add-coins',
   aliases: ['ac'],
   description: 'Add coins to a user',
-  async execute({msg, args}) {
+  async execute({ msg, args }) {
     if (!ownerIds.includes(msg.author.id)) return;
     const user = msg.mentions.members.first();
     if (!user) {
-      return msg.reply('Please mention a user.')
+      return msg.reply('Please mention a user.');
     }
-    maxLimit = 10000000;
+    const maxLimit = 10000000;
     const amount = parseInt(args[1]);
 
     if (!amount || amount < 0 || amount > maxLimit) {
-      return msg.reply(`Please provide a valid amount of coins. You can give 1 to ${maxLimit.toLocaleString()} CP coins`);
+      return msg.reply(`Please provide a valid amount of coins. You can give 1 to ${maxLimit.toLocaleString()} CP coins.`);
     }
 
     const existingUser = await User.findOne({ userId: user.id });
-    if (!existingUser) return msg.reply(`**${user.displayName}** doesn't have a account yet.`);
+    if (!existingUser) return msg.reply(`**${user.displayName}** doesn't have an account yet.`);
 
-    await existingUser.findOneAndUpdate({ userId: user.id }, { $inc: { balance: amount } });
+    await User.updateOne({ userId: user.id }, { $inc: { balance: amount } });
 
     msg.reply(`Successfully added **__${amount.toLocaleString()}__** ${currency} CP coins to **${user.displayName}**'s balance.`);
   }
-}
+};
